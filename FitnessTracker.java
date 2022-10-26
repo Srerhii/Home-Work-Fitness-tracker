9d062b7d596b891d9597f0a62d581c966e363b89
Serhii <Sergukxxx1990@gmail.com>
2022-10-26 22:11:16 +0300

    Пробный комит

---
+++ FitnessTracker.java
@@ -0,0 +1,31 @@
+public class FitnessTracker {
+    String name;
+    int datOfBirth;
+    int monthOfBirth;
+    int yearsOfBirth;
+    String mail;
+    int telNumber;
+    String surname;
+    int weight;
+    int pressure;
+    int numberOfSteps;
+
+    public FitnessTracker(String name, int datOfBirth, int monthOfBirth, int yearsOfBirth,
+                          String mail, int telNumber,
+                          String surname, int weight, int pressure, int numberOfSteps) {
+        this.name = name;
+        this.datOfBirth = datOfBirth;
+        this.monthOfBirth = monthOfBirth;
+        this.yearsOfBirth = yearsOfBirth;
+        this.mail = mail;
+        this.telNumber = telNumber;
+        this.surname = surname;
+        this.weight = weight;
+        this.pressure = pressure;
+        this.numberOfSteps = numberOfSteps;
+    }
+
+
+
+
+}
